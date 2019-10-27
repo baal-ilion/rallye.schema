5203bda4875c5f336d816bb11f193bf4d283e1a8
baal.ilion <baal.ilion@gmail.com>
2019-10-27 17:54:38 +0100

    Creation du service form-scanner-service

---
+++ form-scanner-service/src/main/java/fr/vandriessche/rallyeschema/formscannerservice/FormScannerServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.vandriessche.rallyeschema.formscannerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormScannerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormScannerServiceApplication.class, args);
+	}
+
+}
---
+++ form-scanner-service/src/test/java/fr/vandriessche/rallyeschema/formscannerservice/FormScannerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.vandriessche.rallyeschema.formscannerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormScannerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
