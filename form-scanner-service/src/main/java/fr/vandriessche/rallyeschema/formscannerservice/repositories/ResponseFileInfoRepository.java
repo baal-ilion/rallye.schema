4c0f5da55b07ac0e2e19f02ce48ed6b1e4fc9cc6
baal.ilion <baal.ilion@gmail.com>
2019-11-23 16:44:03 +0100

    Trier les feuilles de reponses

--- form-scanner-service/src/main/java/fr/vandriessche/rallyeschema/formscannerservice/repositories/ResponseFileInfoRepository.java
+++ form-scanner-service/src/main/java/fr/vandriessche/rallyeschema/formscannerservice/repositories/ResponseFileInfoRepository.java
@@ -3,11 +3,13 @@ package fr.vandriessche.rallyeschema.formscannerservice.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import fr.vandriessche.rallyeschema.formscannerservice.entities.ResponseFileInfo;
 
 public interface ResponseFileInfoRepository extends MongoRepository<ResponseFileInfo, String> {
 	List<ResponseFileInfo> findByStageAndPageAndTeamAndActiveIsTrue(Integer stage, Integer page, Integer team);
 
+	@Query(sort = "{ team : 1, stage : 1, page : 1 }")
 	List<ResponseFileInfo> findByStageAndTeamAndActiveIsTrue(Integer stage, Integer team);
 }
