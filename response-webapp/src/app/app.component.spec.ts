303a170cd59356238b79e2efb210af6982987cd2
baal.ilion <baal.ilion@gmail.com>
2020-02-11 23:26:25 +0100

    Renomer form-scanner-webapp en rallye-schema-response-webapp

--- response-webapp/src/app/app.component.spec.ts
+++ response-webapp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'form-scanner-webapp'`, () => {
+  it(`should have as title 'rallye-schema-response-webapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('form-scanner-webapp');
+    expect(app.title).toEqual('rallye-schema-response-webapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('form-scanner-webapp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('rallye-schema-response-webapp app is running!');
   });
 });
