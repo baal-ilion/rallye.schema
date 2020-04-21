057c20c8347f0371313dca63546d4c8adcc85ad8
baal.ilion <baal.ilion@gmail.com>
2020-04-21 12:54:33 +0200

    Ajout du model pour le parametrage des feuilles de réponses

---
+++ rallye-schema-response-webapp/src/app/response-file/param/models/question-page-param.ts
@@ -0,0 +1,5 @@
+import { QuestionParam } from 'src/app/param/models/question-param';
+
+// tslint:disable-next-line: no-empty-interface
+export interface QuestionPageParam extends QuestionParam {
+}
