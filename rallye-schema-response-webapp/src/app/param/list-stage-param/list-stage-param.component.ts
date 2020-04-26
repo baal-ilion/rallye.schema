0fc041ae18466a19aedb65fb5c32f3a1171f2949
baal.ilion <baal.ilion@gmail.com>
2020-04-26 20:51:06 +0200

    Mise en page de la liste du paramétrage des étapes

--- rallye-schema-response-webapp/src/app/param/list-stage-param/list-stage-param.component.ts
+++ rallye-schema-response-webapp/src/app/param/list-stage-param/list-stage-param.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { HalLink } from 'src/app/models/hal-link';
 import { StageParam } from '../models/stage-param';
 import { NewStageParamComponent } from '../new-stage-param/new-stage-param.component';
 import { StageParamService } from '../stage-param.service';
+
+interface StageParamDetail {
+  param: StageParam;
+  nbQuestion: number;
+  nbPerformance: number;
+  nbResponceFile: number;
+}
 
 @Component({
   selector: 'app-list-stage-param',
@@ -10,32 +18,56 @@ import { StageParamService } from '../stage-param.service';
   styleUrls: ['./list-stage-param.component.scss']
 })
 export class ListStageParamComponent implements OnInit {
-  stageParams: StageParam[] = [];
+  stageParamDetails: StageParamDetail[] = [];
 
   constructor(
     private stageParamService: StageParamService,
     private modalService: NgbModal) { }
 
   ngOnInit() {
-    this.stageParamService.getStageParams().subscribe((data) => {
-      this.stageParams = data._embedded.stageParams;
-    }, (error) => {
-      this.stageParams = [];
-    });
+    this.loadStageParamDetails();
   }
 
-  addStageParam() {
-    const modalRef = this.modalService.open(NewStageParamComponent);
-    modalRef.result.then((result: StageParam) => {
-      console.log(result);
-      this.stageParamService.addStageParam(result).subscribe(data => {
-        this.ngOnInit();
-      }, err => {
-        console.log(err);
-        this.ngOnInit();
-      });
-    }).catch((error) => {
+  async loadStageParamDetails() {
+    this.stageParamDetails = [];
+    try {
+      const stageParams = await this.stageParamService.getStageParams().toPromise();
+      for (const stageParam of stageParams._embedded.stageParams) {
+        const stageParamDetail: StageParamDetail = {
+          param: stageParam,
+          nbPerformance: 0,
+          nbQuestion: 0,
+          nbResponceFile: (stageParam._links?.responseFileParams as HalLink[])?.length ?? 0
+        };
+        for (const questionParam of Object.values(stageParam.questionParams)) {
+          if (questionParam.type === 'QUESTION') {
+            stageParamDetail.nbQuestion += 1;
+          } else if (questionParam.type === 'PERFORMANCE') {
+            stageParamDetail.nbPerformance += 1;
+          }
+        }
+        this.stageParamDetails.push(stageParamDetail);
+      }
+    } catch (error) {
       console.log(error);
-    });
+      this.stageParamDetails = [];
+    }
+  }
+
+  async addStageParam() {
+    const modalRef = this.modalService.open(NewStageParamComponent);
+    try {
+      const result: StageParam = await modalRef.result;
+      console.log(result);
+      try {
+        const created = await this.stageParamService.addStageParam(result).toPromise();
+        this.loadStageParamDetails();
+      } catch (error) {
+        console.log(error);
+        this.loadStageParamDetails();
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
